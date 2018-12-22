c47a70034567283d936612749bb75b9f463efd98
tryit4you <45864070+tryit4you@users.noreply.github.com>
2018-12-22 07:16:17 +0700

    Init Restaurent app using RestApi and Angular7

---
+++ RestaurantApp/Data/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Data.Models
+{
+    public class Customer
+    {
+        [Key]
+        public string Id { get; set; }
+        public string  Name { get; set; }
+
+    }
+}
---
+++ RestaurantApp/Data/Models/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Data.Models
+{
+    public class Order
+    {
+        [Key]
+        public string OrderId { get; set; }
+        public string OrderNo { get; set; }
+        public string CustomerId { get; set; }
+        public string PayMethod { get; set; }
+        public decimal  Total { get; set; }
+        [ForeignKey("CustomerId")]
+        public virtual Customer Customer { get; set; }
+
+    }
+}
